ac55d8fd2408c4bd03c08df0977dea1dcfc80661
tlhsieh <tlhsieh@datalab.cs.nthu.edu.tw>
2025-04-23 23:27:30 +0800

    lab 07

---
+++ lib/state/filtered_meals_notifier.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class FilteredMealsNotifier extends ChangeNotifier {
+//congratulations, you've found this secret dart file,
+//maybe implement your filteredMeals here?  
+
+
+}
